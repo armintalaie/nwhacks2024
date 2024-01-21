0886a19fd274198bd7f35a4e4005ed4d3213c33e
Armin Talaie <talaiearmin78@gmail.com>
2024-01-20 19:44:33 -0800

    added challenge page to stack

--- my-turborepo/apps/native/App.tsx
+++ my-turborepo/apps/native/App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/home';
 import Auth from './components/auth';// Your authentication screen
+import Challenge from './components/Challenge';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ const App = () => {
       <Stack.Navigator initialRouteName="Auth">
         <Stack.Screen name="Auth" component={Auth} />
         <Stack.Screen name="Native" component={Home} />
+        <Stack.Screen name="Challenge" component={Challenge} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
