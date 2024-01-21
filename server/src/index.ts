47cd9fae2d81855ce07be786bd78f919e181c2eb
Michael DeMarco <michaelfromyeg@gmail.com>
2024-01-20 22:21:44 -0800

    feat: server listens to port

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,16 @@
-import express from 'express';
-import { goals } from './routes';
+import express from "express";
+import { goals } from "./routes";
 
 const app = express();
 app.use(express.json());
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express with TypeScript!');
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express with TypeScript!");
 });
 
-app.use('/goals', goals);
+app.use("/goals", goals);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
